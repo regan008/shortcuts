ab8e06d67578e3f1588ae00fac6885a032080059
Amanda Regan <regan008@cougars.csusm.edu>
2018-04-01 09:29:09 -0400

    adding terminal shortcut file

---
+++ myshortcuts.zsh
@@ -0,0 +1,14 @@
+alias htdocs="cd /Applications/MAMP/htdocs"
+alias TKDir="/Applications/MAMP/htdocs/TurnKey/wp-content/themes/BrewChildTheme"
+alias htdocs="/Applications/MAMP/htdocs/"
+alias wgh="/Applications/MAMP/htdocs/wgh/"
+alias serve="python -m SimpleHTTPServer 8000"
+
+
+alias hs="hugo serve"
+alias arcom="cd ~/Dropbox/amanda-regan/personalsite"
+alias myserver="ssh amandau9@amanda-regan.com"
+alias h390="cd ~/Dropbox/*Teaching/H390Summer17"
+alias h390s="cd ~/Dropbox/*Teaching/Coursesites/H390Su17 && atom . && hugo serve"
+alias teach="cd ~/Dropbox/*Teaching"
+alias h121="cd ~/Dropbox/*Teaching/Coursesites/H121Su18 && atom . && hugo serve"
